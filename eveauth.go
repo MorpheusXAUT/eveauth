99eee1aee0d966fb3ce32062eba6e113047a8e6b
Nick 'MorpheusXAUT' Mueller <nick@morpheusxaut.net>
2015-01-10 02:31:28 +0100

    Added new "misc" package for general stuff being used by multiple parts of the application

--- eveauth.go
+++ eveauth.go
@@ -2,6 +2,14 @@
 // Beside providing basic user authentication, the application can provide access using different permissions based on access rights.
 package main
 
-func main() {
+import (
+	"github.com/morpheusxaut/eveauth/misc"
+)
 
+func main() {
+	misc.ParseConfigFlags()
+
+	misc.SetupLogger(uint(misc.Config.DebugLevel))
+
+	misc.Logger.Infoln("Logger set up! Continuing initialisation...")
 }
