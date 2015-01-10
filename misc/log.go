831f7d0384e947a50e17dec12733638a58513a66
Nick 'MorpheusXAUT' Mueller <nick@morpheusxaut.net>
2015-01-10 03:26:52 +0100

    Added a WebLogging method to the logger, allowing for performance measuring of web requests

--- misc/log.go
+++ misc/log.go
@@ -2,14 +2,38 @@ package misc
 
 import (
 	"github.com/kdar/factorlog"
+	"net/http"
 	"os"
+	"strings"
+	"time"
 )
 
 var (
 	Logger *factorlog.FactorLog
 )
 
-func SetupLogger(debugLevel uint) {
+func SetupLogger() {
 	Logger = factorlog.New(os.Stdout, factorlog.NewStdFormatter("[%{Date} %{Time}] {%{SEVERITY}:%{File}/%{Function}:%{Line}} %{SafeMessage}"))
-	Logger.SetMinMaxSeverity(factorlog.Severity(1<<debugLevel), factorlog.PANIC)
+	Logger.SetMinMaxSeverity(factorlog.Severity(1<<uint(Config.DebugLevel)), factorlog.PANIC)
+}
+
+func WebLogging(inner http.Handler, name string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		remoteAddr := r.Header.Get("X-Forwarded-For")
+
+		if len(remoteAddr) > 0 {
+			remoteAddrs := strings.Split(remoteAddr, ", ")
+			if len(remoteAddrs) > 1 {
+				r.RemoteAddr = remoteAddrs[0]
+			} else {
+				r.RemoteAddr = remoteAddr
+			}
+		}
+
+		inner.ServeHTTP(w, r)
+
+		Logger.Debugf("WebLogging: [%s] %s %q {%s} - %s ", r.Method, r.RemoteAddr, r.RequestURI, name, time.Since(start))
+	})
 }
