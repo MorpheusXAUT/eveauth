9389343e9c81bed86d36598df3cf943783f91099
Nick 'MorpheusXAUT' Mueller <nick@morpheusxaut.net>
2015-01-12 22:11:03 +0100

    Moved the WebLogger method from log.go to controller.go and renamed it to ServeHTTP

--- misc/log.go
+++ misc/log.go
@@ -2,10 +2,7 @@ package misc
 
 import (
 	"github.com/kdar/factorlog"
-	"net/http"
 	"os"
-	"strings"
-	"time"
 )
 
 var (
@@ -15,25 +12,4 @@ var (
 func SetupLogger(debugLevel int) {
 	Logger = factorlog.New(os.Stdout, factorlog.NewStdFormatter("[%{Date} %{Time}] {%{SEVERITY}:%{File}/%{Function}:%{Line}} %{SafeMessage}"))
 	Logger.SetMinMaxSeverity(factorlog.Severity(1<<uint(debugLevel)), factorlog.PANIC)
-}
-
-func WebLogging(inner http.Handler, name string) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-
-		remoteAddr := r.Header.Get("X-Forwarded-For")
-
-		if len(remoteAddr) > 0 {
-			remoteAddrs := strings.Split(remoteAddr, ", ")
-			if len(remoteAddrs) > 1 {
-				r.RemoteAddr = remoteAddrs[0]
-			} else {
-				r.RemoteAddr = remoteAddr
-			}
-		}
-
-		inner.ServeHTTP(w, r)
-
-		Logger.Debugf("WebLogging: [%s] %s %q {%s} - %s ", r.Method, r.RemoteAddr, r.RequestURI, name, time.Since(start))
-	})
 }
--- web/controller.go
+++ web/controller.go
@@ -1,18 +1,22 @@
 package web
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/morpheusxaut/eveauth/database"
 	"github.com/morpheusxaut/eveauth/misc"
 	"net"
 	"net/http"
 	"strconv"
+	"strings"
+	"time"
 )
 
 type Controller struct {
 	Config   *misc.Configuration
 	Database database.DatabaseConnection
-	router   *mux.Router
+
+	router *mux.Router
 }
 
 func SetupController(config *misc.Configuration, db database.DatabaseConnection) *Controller {
@@ -25,17 +29,33 @@ func SetupController(config *misc.Configuration, db database.DatabaseConnection)
 	routes := SetupRoutes(controller)
 
 	for _, route := range routes {
-		var handler http.Handler
-
-		handler = route.HandlerFunc
-		handler = misc.WebLogging(handler, route.Name)
-
-		controller.router.Methods(route.Methods...).Path(route.Pattern).Name(route.Name).Handler(handler)
+		controller.router.Methods(route.Methods...).Path(route.Pattern).Name(route.Name).Handler(controller.ServeHTTP(route.HandlerFunc, route.Name))
 	}
 
 	controller.router.PathPrefix("/").Handler(http.FileServer(http.Dir("./web/assets")))
 
 	return controller
+}
+
+func (controller *Controller) ServeHTTP(inner http.Handler, name string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		remoteAddr := r.Header.Get("X-Forwarded-For")
+
+		if len(remoteAddr) > 0 {
+			remoteAddrs := strings.Split(remoteAddr, ", ")
+			if len(remoteAddrs) > 1 {
+				r.RemoteAddr = fmt.Sprintf("%s:0", remoteAddrs[0])
+			} else {
+				r.RemoteAddr = fmt.Sprintf("%s:0", remoteAddr)
+			}
+		}
+
+		inner.ServeHTTP(w, r)
+
+		misc.Logger.Debugf("ServeHTTP: [%s] %s %q {%s} - %s ", r.Method, r.RemoteAddr, r.RequestURI, name, time.Since(start))
+	})
 }
 
 func (controller *Controller) HandleRequests() {
