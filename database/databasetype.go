31f60750535a7c8d8d6ba635d1b2ebc6028ba2c4
Nick 'MorpheusXAUT' Mueller <nick@morpheusxaut.net>
2015-01-12 17:10:15 +0100

    Added more DatabaseTypes to enumerations Added string representation for DatabaseTypes

--- database/databasetype.go
+++ database/databasetype.go
@@ -3,5 +3,20 @@ package database
 type DatabaseType int
 
 const (
-	DatabaseTypeMySQL DatabaseType = iota
+	DatabaseTypeMock DatabaseType = iota - 1
+	DatabaseTypeMemory
+	DatabaseTypeMySQL
 )
+
+func (t DatabaseType) String() string {
+	switch t {
+	case DatabaseTypeMock:
+		return "Mock"
+	case DatabaseTypeMemory:
+		return "Memory"
+	case DatabaseTypeMySQL:
+		return "MySQL"
+	default:
+		return "Unknown"
+	}
+}
