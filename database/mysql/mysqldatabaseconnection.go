40110046f886f3ca6dd7fe26f6b427722a3634e4
Nick 'MorpheusXAUT' Mueller <nick@morpheusxaut.net>
2015-01-14 18:01:42 +0100

    Added implementation for all current database interface methods to mysqldatabaseconnection.go (still missing a few single load-statements)

--- database/mysql/mysqldatabaseconnection.go
+++ database/mysql/mysqldatabaseconnection.go
@@ -33,11 +33,6 @@ func (c *MySQLDatabaseConnection) Connect() error {
 }
 
 func (c *MySQLDatabaseConnection) RawQuery(query string, v ...interface{}) ([]map[string]interface{}, error) {
-	err := c.conn.Ping()
-	if err != nil {
-		return nil, err
-	}
-
 	rows, err := c.conn.Query(query, v...)
 	if err != nil {
 		return nil, err
@@ -70,14 +65,9 @@ func (c *MySQLDatabaseConnection) RawQuery(query string, v ...interface{}) ([]ma
 }
 
 func (c *MySQLDatabaseConnection) LoadAllAPIKeys() ([]*models.APIKey, error) {
-	err := c.conn.Ping()
-	if err != nil {
-		return nil, err
-	}
-
 	apiKeys := make([]*models.APIKey, 0)
 
-	err = c.conn.Select(&apiKeys, "SELECT id, userid, apikeyid, apivcode, active FROM userapikeys")
+	err := c.conn.Select(&apiKeys, "SELECT id, userid, apikeyid, apivcode, active FROM userapikeys;")
 	if err != nil {
 		return nil, err
 	}
@@ -86,25 +76,117 @@ func (c *MySQLDatabaseConnection) LoadAllAPIKeys() ([]*models.APIKey, error) {
 }
 
 func (c *MySQLDatabaseConnection) LoadAllCorporations() ([]*models.Corporation, error) {
-	return nil, misc.ErrNotImplemented
+	corporations := make([]*models.Corporation, 0)
+
+	err := c.conn.Select(&corporations, "SELECT id, name, ticker, evecorporationid, apikeyid, apivcode, active FROM corporations;")
+	if err != nil {
+		return nil, err
+	}
+
+	return corporations, nil
 }
 
 func (c *MySQLDatabaseConnection) LoadAllCharacters() ([]*models.Character, error) {
-	return nil, misc.ErrNotImplemented
+	characters := make([]*models.Character, 0)
+
+	err := c.conn.Select(&characters, "SELECT id, userid, corporationid, name, evecharacterid, active FROM characters;")
+	if err != nil {
+		return nil, err
+	}
+
+	return characters, nil
 }
 
-func (c *MySQLDatabaseConnection) LoadAllGroupRoles() ([]*models.Role, error) {
-	return nil, misc.ErrNotImplemented
+func (c *MySQLDatabaseConnection) LoadAllGroupRoles() ([]*models.GroupRole, error) {
+	groupRoles := make([]*models.GroupRole, 0)
+
+	rows, err := c.conn.Queryx("SELECT id, groupid, roleid, autoadded, granted FROM grouproles;")
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		var id, groupID, roleID int64
+		var autoadded, granted int
+
+		err = rows.Scan(&id, &groupID, &roleID, &autoadded, &granted)
+		if err != nil {
+			return nil, err
+		}
+
+		// TODO load role from database
+
+		groupRole := &models.GroupRole{
+			ID:        id,
+			GroupID:   groupID,
+			Role:      nil,
+			AutoAdded: (autoadded != 0),
+			Granted:   (granted != 0),
+		}
+
+		groupRoles = append(groupRoles, groupRole)
+	}
+
+	return groupRoles, nil
 }
 
-func (c *MySQLDatabaseConnection) LoadAllUserRoles() ([]*models.Role, error) {
-	return nil, misc.ErrNotImplemented
+func (c *MySQLDatabaseConnection) LoadAllUserRoles() ([]*models.UserRole, error) {
+	userRoles := make([]*models.UserRole, 0)
+
+	rows, err := c.conn.Queryx("SELECT id, userid, roleid, autoadded, granted FROM userroles;")
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		var id, userID, roleID int64
+		var autoadded, granted int
+
+		err = rows.Scan(&id, &userID, &roleID, &autoadded, &granted)
+		if err != nil {
+			return nil, err
+		}
+
+		// TODO load role from database
+
+		userRole := &models.UserRole{
+			ID:        id,
+			UserID:    userID,
+			Role:      nil,
+			AutoAdded: (autoadded != 0),
+			Granted:   (granted != 0),
+		}
+
+		userRoles = append(userRoles, userRole)
+	}
+
+	return userRoles, nil
 }
 
 func (c *MySQLDatabaseConnection) LoadAllGroups() ([]*models.Group, error) {
-	return nil, misc.ErrNotImplemented
+	groups := make([]*models.Group, 0)
+
+	err := c.conn.Select(&groups, "SELECT id, name, active FROM groups;")
+	if err != nil {
+		return nil, err
+	}
+
+	return groups, nil
 }
 
 func (c *MySQLDatabaseConnection) LoadAllUsers() ([]*models.User, error) {
-	return nil, misc.ErrNotImplemented
+	users := make([]*models.User, 0)
+
+	err := c.conn.Select(&users, "SELECT id, username, password, active FROM users;")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, user := range users {
+		user.UserRoles = make([]*models.UserRole, 0)
+		user.GroupRoles = make([]*models.GroupRole, 0)
+		user.Characters = make([]*models.Character, 0)
+	}
+
+	return users, nil
 }
