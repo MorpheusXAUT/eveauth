9c60e76c814787e24d2f6beb9e4bd731e35aa6a2
Nick 'MorpheusXAUT' Mueller <nick@morpheusxaut.net>
2015-01-14 18:00:55 +0100

    Removed string variables with appropriate sql.NullStrings for required fields in corporation.go and user.go

--- models/corporation.go
+++ models/corporation.go
@@ -1,11 +1,15 @@
 package models
 
+import (
+	"database/sql"
+)
+
 type Corporation struct {
-	ID               int64  `json:"id"`
-	Name             string `json:"name"`
-	Ticker           string `json:"ticker"`
-	EVECorporationID int64  `json:"eveCorporationID"`
-	APIKeyID         int64  `json:"apiKeyID"`
-	APIvCode         string `json:"apivCode"`
-	Active           bool   `json:"active"`
+	ID               int64          `json:"id"`
+	Name             string         `json:"name"`
+	Ticker           string         `json:"ticker"`
+	EVECorporationID int64          `json:"eveCorporationID"`
+	APIKeyID         sql.NullInt64  `json:"apiKeyID"`
+	APIvCode         sql.NullString `json:"apivCode"`
+	Active           bool           `json:"active"`
 }
--- models/user.go
+++ models/user.go
@@ -1,11 +1,15 @@
 package models
 
+import (
+	"database/sql"
+)
+
 type User struct {
-	ID         int64        `json:"id"`
-	Username   string       `json:"username"`
-	Password   string       `json:"password"`
-	Active     bool         `json:"active"`
-	UserRoles  []*UserRole  `json:"userRoles,omitempty"`
-	GroupRoles []*GroupRole `json:"groupRoles,omitempty"`
-	Characters []*Character `json:"characters,omitempty"`
+	ID         int64          `json:"id"`
+	Username   string         `json:"username"`
+	Password   sql.NullString `json:"password"`
+	Active     bool           `json:"active"`
+	UserRoles  []*UserRole    `json:"userRoles,omitempty"`
+	GroupRoles []*GroupRole   `json:"groupRoles,omitempty"`
+	Characters []*Character   `json:"characters,omitempty"`
 }
