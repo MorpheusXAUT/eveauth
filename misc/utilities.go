4193fae08b57a4d2c4428057d497657244cb01c6
Nick 'MorpheusXAUT' Mueller <nick@morpheusxaut.net>
2015-01-21 00:45:08 +0100

    Added utilities file for various utility methods

---
+++ misc/utilities.go
@@ -0,0 +1,17 @@
+package misc
+
+import (
+	"math/rand"
+)
+
+func GenerateRandomString(length int) string {
+	chars := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+	b := make([]rune, length)
+
+	for i := range b {
+		b[i] = chars[rand.Intn(len(chars))]
+	}
+
+	return string(b)
+}
