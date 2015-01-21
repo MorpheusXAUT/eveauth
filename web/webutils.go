ca322fe1cb46a9ad8f90c880f8bdd0d293c702bc
Nick 'MorpheusXAUT' Mueller <nick@morpheusxaut.net>
2015-01-21 15:36:37 +0100

    Added method for sending raw error messages to the client

--- web/webutils.go
+++ web/webutils.go
@@ -2,6 +2,7 @@ package web
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/morpheusxaut/eveauth/misc"
 	"net/http"
 	"strconv"
@@ -10,17 +11,28 @@ import (
 func (controller *Controller) SendResponse(w http.ResponseWriter, r *http.Request, template string, response map[string]interface{}) {
 	err := controller.Templates.ExecuteTemplates(w, r, template, response)
 	if err != nil {
-		// TODO Display error to client
 		misc.Logger.Warnf("Failed to execute template %q: [%v]", template, err)
+		controller.SendRawError(w, http.StatusInternalServerError, err)
 		return
 	}
+}
+
+func (controller *Controller) SendRawError(w http.ResponseWriter, statusCode int, err error) {
+	errorMessage := []byte(fmt.Sprintf("Received fatal error during operation: [%v]", err))
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.Header().Set("Content-Length", strconv.Itoa(len(errorMessage)))
+
+	w.WriteHeader(statusCode)
+
+	w.Write(errorMessage)
 }
 
 func (controller *Controller) SendJSONResponse(w http.ResponseWriter, r *http.Request, response map[string]interface{}) {
 	responseContent, err := json.Marshal(response)
 	if err != nil {
-		// TODO Display error to client
 		misc.Logger.Warnf("Failed to marshal JSON response: [%v]", err)
+		controller.SendRawError(w, http.StatusInternalServerError, err)
 		return
 	}
 
