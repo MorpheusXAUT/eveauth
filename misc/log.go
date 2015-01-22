b0de90c5c411bb57e8fafc7b75a3997b1a4c2592
Nick 'MorpheusXAUT' Mueller <nick@morpheusxaut.net>
2015-01-23 00:44:40 +0100

    Slightly adapted logger output to differentiate between same package names

--- misc/log.go
+++ misc/log.go
@@ -11,6 +11,6 @@ var (
 )
 
 func SetupLogger(debugLevel int) {
-	Logger = factorlog.New(os.Stdout, factorlog.NewStdFormatter("[%{Date} %{Time}] {%{SEVERITY}:%{File}/%{Function}:%{Line}} %{SafeMessage}"))
+	Logger = factorlog.New(os.Stdout, factorlog.NewStdFormatter("[%{Date} %{Time}] {%{SEVERITY}:%{File}/%{PkgFunction}:%{Line}} %{SafeMessage}"))
 	Logger.SetMinMaxSeverity(factorlog.Severity(1<<uint(debugLevel)), factorlog.PANIC)
 }
