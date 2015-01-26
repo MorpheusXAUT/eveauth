c026f80b1dc95d910b3c213a20d214927312fa9d
Nick 'MorpheusXAUT' Mueller <nick@morpheusxaut.net>
2015-01-26 23:02:34 +0100

    Started adding JavaScript code for perform AJAX requests for managing API keys

---
+++ web/assets/js/settingsapikeys.js
@@ -0,0 +1,22 @@
+$(document).ready(function(e) {
+	$('a.api-key-add').click(function() {
+		$.ajax({
+			accepts: "application/json",
+			cache: false,
+			data: $('#apiKeyAdd').serialize(),
+			dataType: "json",
+			error: displayAjaxError,
+			success: function(response) {
+				console.log(response)
+				if (response.success === true && response.error === null) {
+					location.reload(true);
+				} else {
+					displayError(response.error);
+				}
+			},
+			timeout: 10000,
+			type: "PUT",
+			url: "/settings/apikeys"
+		});
+	});
+});
