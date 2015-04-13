7b5e0c57c21a851c24a8d9b85458b172fb6c4700
Nick 'MorpheusXAUT' Mueller <nick@morpheusxaut.net>
2015-04-13 03:28:04 +0200

    Added button and JS to toggle a role granted state (granted, denied) to the user details page (no backend implementation yet)

--- app/assets/js/adminuserdetails.js
+++ app/assets/js/adminuserdetails.js
@@ -13,6 +13,20 @@ $(document).ready(function(e) {
 		});
 	});
 
+	$('a.admin-userdetails-role-toggle-granted').click(function() {
+		$.ajax({
+			accepts: "application/json",
+			cache: false,
+			data: "command=adminUserDetailsRoleToggleGranted&userID="+$(this).attr('userID')+"&roleID="+$(this).attr('roleID')+"&csrfToken="+$(this).attr('csrfToken'),
+			dataType: "json",
+			error: displayAjaxError,
+			success: displayResponse,
+			timeout: 10000,
+			type: "PUT",
+			url: "/admin/users"
+		});
+	});
+
 	$('a.admin-userdetails-role-delete').click(function() {
 		$.ajax({
 			accepts: "application/json",
