dcc72f6ad5e5457dc83620bd3ad7e9661d0af835
Nick 'MorpheusXAUT' Mueller <nick@morpheusxaut.net>
2015-04-13 03:30:34 +0200

    Added button and JS to toggle a role granted state (granted, denied) to the group details page (no backend implementation yet)

--- app/assets/js/admingroupdetails.js
+++ app/assets/js/admingroupdetails.js
@@ -1,4 +1,18 @@
 $(document).ready(function(e) {
+	$('a.admin-groupdetails-role-toggle-granted').click(function() {
+		$.ajax({
+			accepts: "application/json",
+			cache: false,
+			data: "command=adminGroupDetailsRoleToggleGranted&groupID="+$(this).attr('groupID')+"&roleID="+$(this).attr('roleID')+"&csrfToken="+$(this).attr('csrfToken'),
+			dataType: "json",
+			error: displayAjaxError,
+			success: displayResponse,
+			timeout: 10000,
+			type: "PUT",
+			url: "/admin/groups"
+		});
+	});
+	
 	$('a.admin-groupdetails-role-delete').click(function() {
 		$.ajax({
 			accepts: "application/json",
